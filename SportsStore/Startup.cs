0e6c5564d9262e3c6fdb8a58050ccb6cfd396496
Kehinde Akeredolu <kayzkenny@gmail.com>
2020-06-09 13:24:53 +0100

    Complete all cart features

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -29,9 +29,12 @@ namespace SportsStore
                 Configuration["ConnectionStrings:SportsStoreConnection"]);
             });
             services.AddScoped<IStoreRepository, EFStoreRepository>();
+            services.AddScoped<IOrderRepository, EFOrderRepository>();
             services.AddRazorPages();
             services.AddDistributedMemoryCache();
             services.AddSession();
+            services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
